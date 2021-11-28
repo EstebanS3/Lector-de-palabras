1e13dcc8da27cbae8122c68046e667c0fc4dd0c9
EstebanS3 <dragon.elias@gmail.com>
2021-11-27 22:14:22 -0600

    3

--- Lector_de_palabra.py
+++ Lector_de_palabra.py
@@ -1,7 +1,37 @@
 print("Bienvenido al contador de palabras por texto")
-palabra=[]
-with open("Texto.txt") as fname:
-    for linea in fname:
-        palabra.extend(linea.split())
-print(palabra)
-print("Hasta Luego , Vuelva pronto")
+palabras=[]
+with open("Ejemplo.txt") as fname:
+    for palabra in fname:
+        palabras.extend(palabra.split())
+        print("estas son las palbras separadas por comas")
+print(palabras)
+print("Pasamos a la fase 2 del programa")
+print("Ahora se procede al diccionario")
+
+archivo = open('Ejemplo.txt', 'r')
+palabraHash = archivo.read()
+
+if palabraHash is not None:
+            palabraHash = palabraHash.replace(",", "")
+            palabraHash = palabraHash.replace(".", "")
+
+def printFrequency(palabraHash):
+    List = {}
+    word = ""
+    for i in range(len(palabraHash)):
+        if (palabraHash[i] == ' '):
+            if (word not in List):
+                List[word] = 1
+                word = ""
+            else:
+                List[word] += 1
+                word = ""
+        else:
+            word += palabraHash[i]
+    if (word not in List):
+        List[word] = 1
+    else:
+        List[word] += 1
+    for i in List:
+        print(f"{i}->{List[i]}")
+printFrequency(palabraHash)
