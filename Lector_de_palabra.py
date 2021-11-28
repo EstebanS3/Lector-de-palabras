a2adf665b3e6087dd96793eb2733183801ecfcf8
EstebanS3 <dragon.elias@gmail.com>
2021-11-27 19:56:07 -0600

    1

--- Lector_de_palabra.py
+++ Lector_de_palabra.py
@@ -1,1 +1,7 @@
-
+print("Bienvenido al contador de palabras por texto")
+palabra=[]
+with open("Texto.txt") as fname:
+    for linea in fname:
+        palabra.extend(linea.split())
+print(palabra)
+print("Hasta Luego , Vuelva pronto")
